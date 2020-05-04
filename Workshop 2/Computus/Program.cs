c3c11ad079104846ec155829241ff00950039052
Clyde Davies <clyde.davies@gmail.com>
2020-05-04 10:05:55 +0100

    Computus algorithm added

---
+++ Workshop 2/Computus/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 0;
+            const int M = 24, N = 5;
+            string response = "";
+            do
+            {
+                Console.Write("Enter the year:");
+                response = Console.ReadLine();
+                if (int.TryParse(response, out year))
+                {
+                    int a = year % 19;
+                    int b = year % 4;
+                    int c = a % 7;
+                    int d = (19 * a + M) % 30;
+                    int e = (2 * b + 4 * c + 6 * d + N) % 7;
+                    if (d + e < 10)
+                    {
+                        Console.WriteLine("Easter falls on March {0} ", (d + e + 22));
+                    }
+                    else
+                    {
+                        int date = d + e - 9;
+                        if (date == 26)
+                        {
+                            date = 19;
+                        }
+                        else if (date == 25 & d == 28 & e == 6 & a > 10)
+                        {
+                            date = 18;
+                        }
+                        Console.WriteLine("Easter falls on April {0} ", date);
+                    }
+                }
+            } while (response != "");
+        }
+    }
+}
---
+++ Workshop 2/bad expression/bad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit = 170;
+            int celsius = fahrenheit - 32 * 5 / 9;
+
+            if (celsius >= 100)
+                Console.WriteLine("boiling");
+            else if (celsius > 0)
+                Console.WriteLine("Just right");
+            else
+                Console.WriteLine("freezing");
+        }
+    }
+}
